74f4ac34c048126575da79f080f4a308cb915438
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:28:26 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дан файл numsTask3.txt с целыми числами, вычислите среднее арифметическое элементов расположенных до минимального;
+
+using System;
+using System.IO;
+using System.Linq;
+class work_5_3
+{
+    static void Main()
+    {
+        string[] use = File.ReadAllLines("numsTask3.txt"); 
+        int[] chosenUse = use[0].Split(' ').Select(int.Parse).ToArray(); 
+
+        int minIndex = Array.IndexOf(chosenUse, chosenUse.Min()); 
+        double averageBeforeMin = chosenUse.Take(minIndex).Average(); 
+
+        Console.WriteLine("Среднее арифметическое до минимального числа: " + averageBeforeMin); 
+    }
+}
